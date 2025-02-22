e878033cc031b92d5bf0fbdd94d6b7599af25a54
RaphaellArcanjo <raphaellarcanjo@proton.me>
2025-02-22 19:52:10 -0300

    Exercicio 1 - Ola Mundo

---
+++ src/ComecandoDoZero/exercicio1.java
@@ -0,0 +1,19 @@
+package ComecandoDoZero;
+/*
+* Esse é o primeiro problema que você deve resolver. Normalmente problemas terão uma entrada e uma saída esperada, mas por enquanto vamos começar com um
+* problema bem simples, onde você deve apenas imprimir uma mensagem na tela do computador. Dessa forma, para resolver esse problema você deve apenas imprimir
+* a mensagem "Ola Mundo!" na tela do computador. Para enviar o código contendo sua solução, você deve usar os botões que se encontram no topo dessa página.
+* O botão para enviar sua solução é identificado pelo símbolo do avião de papel.
+
+* Entrada
+Nesse problema especial você não precisa ler nenhuma informação como entrada.
+
+Saída
+A saída do seu programa deve ser apenas a frase "Ola Mundo!" (sem as aspas). Não se esqueça de adicionar o sinal de exclamação no final.
+* */
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+}
