015dc4e3ec36f00279e06feeba53db8ebd2596af
RaphaellArcanjo <raphaellarcanjo@proton.me>
2025-02-25 22:54:06 -0300

    Exercicio 9 - Bondinho

---
+++ src/EstruturasCondicionais/exercicio9.java
@@ -0,0 +1,31 @@
+package EstruturasCondicionais;
+
+import java.util.Scanner;
+
+/*A turma do colégio vai fazer uma excursão na serra e todos os alunos e monitores vão tomar um bondinho para subir até o pico de uma montanha.
+A cabine do bondinho pode levar 50 pessoas no máximo, contando alunos e monitores, durante uma viagem até o pico. Neste problema, dado como entrada o número de a
+lunos A e o número de monitores M, você deve escrever um programa que diga se é possível ou não levar todos os alunos e monitores em apenas uma viagem!
+
+Entrada:
+A primeira linha da entrada contém um inteiro A, representando a quantidade de alunos. A segunda linha da entrada contém um inteiro M, representando o número de monitores.
+
+Saída: Seu programa deve imprimir uma linha contendo o caractere S se é possível levar todos os alunos e monitores em apenas uma viagem, ou o caractere N caso não
+seja possível. */
+
+public class exercicio9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int M = sc.nextInt();
+
+        int total = A + M;
+        if(total <= 50){
+            System.out.println("S");
+        } else{
+            System.out.println("N");
+        }
+
+        sc.close();
+    }
+}
